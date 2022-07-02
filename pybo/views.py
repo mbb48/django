e3fbfa47d1dd59230d0893c120d34d69f3baba61
yellowbird8201@gmail.com <yellowbird8201@gmail.com>
2022-07-02 16:43:26 +0900

    16:43

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,11 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from pybo.models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse('GO to hell django lol')
+    question_list=Question.objects.order_by('-create_data')
+    context={'question_list':question_list}
+    return render(request, 'pybo/question_list.html', context)
+
