6f87d6a12e9055cbae3fea24b5091d9a908c5e8e
yellowbird8201@gmail.com <yellowbird8201@gmail.com>
2022-07-02 14:52:03 +0900

    14:52

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    subject=models.CharField(max_length=200)
+    content=models.TextField()
+    create_data=models.DateTimeField()
+class Answer(models.Model):
+    question=models.ForeignKey(Question, on_delete=models.CASCADE)
+    content=models.TextField()
+    create_data=models.DateTimeField()
